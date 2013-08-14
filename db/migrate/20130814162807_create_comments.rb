6b57206f86064804af92bae61ec55c49292bc2f9
dbc-apprentice <dbc@dbc.com>
2013-08-14 16:56:01 -0700

    fix migration bug

--- db/migrate/20130814162807_create_comments.rb
+++ db/migrate/20130814162807_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def change
-    create_table do |t|
+    create_table :comments do |t|
       t.string :content
       t.belongs_to :user
       t.belongs_to :post
